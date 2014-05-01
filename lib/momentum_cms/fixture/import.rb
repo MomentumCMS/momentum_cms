e390b8eb54ae97fb63e61b5431dff93c5a5f5f20
Emerson Lackey <emerson.lackey@gmail.com>
2014-05-01 01:48:44 -0400

    Import tests are passing, but the code needs work
    This implementation is by no means correct.

--- lib/momentum_cms/fixture/import.rb
+++ lib/momentum_cms/fixture/import.rb
@@ -10,19 +10,16 @@ class MomentumCms::Fixture
     end
 
     def self.pages(site, path, parent = nil)
-      pages = []
-      puts Dir["#{path}*/"].inspect
       Dir["#{path}*/"].each do |path|
         if File.exists?(attrs_path = File.join(path, 'attributes.json'))
 
           # Fetch the attributes
           attributes = MomentumCms::Fixture::Utils.read_json(attrs_path)
 
-          puts "=================================="
-          puts "Path: #{path}"
-          puts "Parent: #{parent.inspect}"
-          puts "Attributes: #{attributes}"
-          puts ""
+          # Determine the parent
+          unless MomentumCms::Fixture::Import.is_parent?(parent, path)
+            parent = MomentumCms::Fixture::Import.get_parent(path)
+          end
 
           # Build the page
           page = site.pages.build(
@@ -39,6 +36,28 @@ class MomentumCms::Fixture
           MomentumCms::Fixture::Import.pages(site, path, parent)
 
         end
+      end
+    end
+
+    def self.is_parent?(page, path)
+      parent_path = File.expand_path('..', path)
+      if File.exists?(attrs_path = File.join(parent_path, 'attributes.json'))
+        attributes = MomentumCms::Fixture::Utils.read_json(attrs_path)
+        if attributes['slug'].present? && page.slug == attributes['slug']
+          is_parent = true
+        end
+      end
+      is_parent
+    end
+
+    # TODO: I believe there is a situation where pages with duplicate slugs
+    # could result in a malformed tree, although the tests seem to pass
+    # without issue
+    def self.get_parent(path)
+      parent_path = File.expand_path('..', path)
+      if File.exists?(attrs_path = File.join(parent_path, 'attributes.json'))
+        attributes = MomentumCms::Fixture::Utils.read_json(attrs_path)
+        return MomentumCms::Page.find_by(slug: attributes['slug'])
       end
     end
 
--- test/lib/fixture/import_test.rb
+++ test/lib/fixture/import_test.rb
@@ -16,7 +16,7 @@ class FixtureImportTest < ActiveSupport::TestCase
 
   def test_basic_page_tree_import
     MomentumCms::Page.destroy_all
-    assert_difference "MomentumCms::Page.count", 4 do
+    assert_difference "MomentumCms::Page.count", 8 do
       MomentumCms::Fixture::Import.site(@path)
     end
     # Home Page
@@ -26,9 +26,21 @@ class FixtureImportTest < ActiveSupport::TestCase
     # About Page
     about = home.children.find_by(label: 'About')
     assert_equal 'about', about.slug
+    # Portfolio Page
+    portfolio = about.children.find_by(label: 'Portfolio')
+    assert_equal 'portfolio', portfolio.slug
+    # Client A Page
+    client_a = portfolio.children.find_by(label: 'Client A')
+    assert_equal 'client-a', client_a.slug
     # Contact Page
     contact = home.children.find_by(label: 'Contact')
     assert_equal 'contact', contact.slug
+    # Services Page
+    services = home.children.find_by(label: 'Services')
+    assert_equal 'services', services.slug
+    # Services About Page
+    services_about = services.children.find_by(label: 'Services About')
+    assert_equal 'about', services_about.slug
   end
 
 end
