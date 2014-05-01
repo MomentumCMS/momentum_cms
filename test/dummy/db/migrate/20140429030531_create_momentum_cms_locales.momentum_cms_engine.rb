d039e960c77db945e8108ee3445f3e862f584839
Bill Li <me@billxinli.com>
2014-05-01 12:46:43 -0400

    Remove test dummy migrations

--- test/dummy/db/migrate/20140429030531_create_momentum_cms_locales.momentum_cms_engine.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from momentum_cms_engine (originally 20140426214053)
-class CreateMomentumCmsLocales < ActiveRecord::Migration
-  def change
-    create_table :momentum_cms_locales do |t|
-      t.string :label
-      t.string :identifier
-    end
-  end
-end
