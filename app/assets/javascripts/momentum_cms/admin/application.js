c46dfaefab3e679cc1bfc7c597689f5957a9a662
Bill Li <me@billxinli.com>
2014-05-05 12:07:36 -0400

    Changing some options for the editor.

--- app/assets/javascripts/momentum_cms/admin/application.js
+++ app/assets/javascripts/momentum_cms/admin/application.js
@@ -2,5 +2,6 @@
 //= require jquery_ujs
 //= require vendor/bootstrap
 //= require_tree ../../vendor/ace
+//= require vendor/emmet
 //= require momentum_cms/admin/util/ace
 //= require_self
--- app/assets/javascripts/momentum_cms/admin/util/ace.js
+++ app/assets/javascripts/momentum_cms/admin/util/ace.js
@@ -13,12 +13,49 @@ $(function () {
     }).insertBefore(textarea);
 
     textarea.css('visibility', 'hidden').css('height', '0px');
+    
+    ace.require("ace/ext/old_ie");
+    ace.require("ace/ext/language_tools");
+    ace.require("ace/ext/emmet");
 
     var editor = ace.edit(editDiv[0]);
-//    editor.renderer.setShowGutter(false);
+    ace.require('ace/ext/settings_menu').init(editor);
+
     editor.getSession().setValue(textarea.val());
     editor.getSession().setMode("ace/mode/" + mode);
     editor.setTheme("ace/theme/github");
+
+    editor.setAutoScrollEditorIntoView();
+    editor.setOption("minLines", 10);
+    editor.setOption("maxLines", 50);
+    editor.setOption("enableEmmet", true);
+    editor.setOptions({
+      enableBasicAutocompletion: true,
+      enableSnippets: true
+    });
+    
+    editor.commands.addCommands([
+      {
+        name: "showSettingsMenu",
+        bindKey: {win: "Ctrl-e", mac: "Command-e"},
+        exec: function (editor) {
+          editor.showSettingsMenu();
+        },
+        readOnly: true
+      }
+    ]);
+
+    editor.commands.addCommand({
+      name: "showKeyboardShortcuts",
+      bindKey: {win: "Ctrl-d", mac: "Command-d"},
+      exec: function (editor) {
+        ace.config.loadModule("ace/ext/keybinding_menu", function (module) {
+          module.init(editor);
+          editor.showKeyboardShortcuts()
+        })
+      }
+    });
+
 
     // copy back to textarea on form submit...
     textarea.closest('form').submit(function () {
