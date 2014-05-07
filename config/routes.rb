017a30e41d99aa932389e8b9b3ed273cc00ed4c2
Bill Li <me@billxinli.com>
2014-05-07 14:54:32 -0400

    Making the Admin panel revolve around the current_site

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  namespace :momentum_cms, path: '/' do
-    namespace :admin do
-      root to: 'dashboards#index'
-      resources :sites
-      resources :templates
-      resources :files
-      resources :pages do
-        resources :contents
+  namespace :momentum_cms, as: :cms, path: '/' do
+    namespace :admin, as: :admin, except: :show do
+      root to: 'dashboards#selector'
+      resources :sites do
+        resources :dashboards, only: [:index]
+        resources :templates
+        resources :files
+        resources :pages do
+          resources :contents
+        end
       end
+      get 'sites/:id', to: 'dashboards#selector'
     end
-    
     get '*id', to: 'contents#show'
     root to: 'contents#show'
   end
