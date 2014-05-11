eb4e19c179073f6f5dabf0fe9bde4ef8b46be17c
Bill Li <me@billxinli.com>
2014-05-11 12:49:01 -0400

    Tests

--- test/models/momentum_cms/site_test.rb
+++ test/models/momentum_cms/site_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class MomentumCms::SiteTest < ActiveSupport::TestCase
+  def setup
+    @site = momentum_cms_sites(:default)
+    MomentumCms::Site.class_eval do
+      has_settings do |s|
+        s.key :site, :defaults => { :title => 'Just another CMS' }
+        s.key :language, :defaults => { :locales => [] }
+      end
+    end
+  end
+
   def test_fixture_validity
     MomentumCms::Site.all.each do |site|
       assert site.valid?
@@ -8,12 +18,42 @@ class MomentumCms::SiteTest < ActiveSupport::TestCase
   end
 
   def test_create
-    assert_difference "MomentumCms::Site.count" do
+    assert_difference 'MomentumCms::Site.count' do
       MomentumCms::Site.create(
         host: 'foo.host',
         label: 'Site Name',
         identifier: 'test'
       )
     end
+    assert_no_difference 'MomentumCms::Site.count' do
+      MomentumCms::Site.create(
+        host: 'foo.host',
+        label: 'Site Name',
+        identifier: 'test'
+      )
+    end
+  end
+
+  def test_assign_identifier
+    assert_difference 'MomentumCms::Site.count' do
+      MomentumCms::Site.create(
+        host: 'foo.host',
+        label: 'Site Name'
+      )
+    end
+  end
+
+  def test_site_languages
+    @site = MomentumCms::Site.create(
+      host: 'foo.host',
+      label: 'Site Name'
+    )
+    assert_equal @site.get_locales, []
+    assert_equal @site.get_locales([:en]), [:en]
+    @site.settings(:language).locales = [:en, :fr]
+    @site.save!
+    @site.reload
+    assert_equal @site.get_locales, [:en, :fr]
+    assert_equal @site.get_locales([:en]), [:en, :fr]
   end
 end
--- test/models/momentum_cms/snippet_test.rb
+++ test/models/momentum_cms/snippet_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class MomentumCms::SnippetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @site = momentum_cms_sites(:default)
+  end
+
+  def test_fixture_validity
+    MomentumCms::Snippet.all.each do |snippet|
+      assert snippet.valid?
+    end
+  end
+
+  def test_create
+    assert_difference 'MomentumCms::Snippet.count' do
+      MomentumCms::Snippet.create(
+        label: 'Snippet Name',
+        slug: 'slug',
+        site: @site
+      )
+    end
+    assert_no_difference 'MomentumCms::Snippet.count' do
+      MomentumCms::Snippet.create(
+        label: 'Snippet Name',
+        slug: 'slug',
+        site: @site
+      )
+    end
+  end
+
+  def test_assign_identifier
+    assert_difference 'MomentumCms::Snippet.count' do
+      MomentumCms::Snippet.create(
+        label: 'Snippet Name',
+        site: @site
+      )
+    end
+  end
 end
