1156f45a2c37e608caf1c614b0ae9928f4c203c4
Emerson Lackey <emerson.lackey@gmail.com>
2014-05-21 23:45:55 -0400

    updating schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521010121) do
+ActiveRecord::Schema.define(version: 20140519191635) do
 
   create_table "momentum_cms_block_templates", force: true do |t|
     t.integer  "template_id"
